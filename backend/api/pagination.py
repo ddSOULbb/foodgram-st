3268578b607df76dd9b8f995dd62a08c19d89729
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-21 11:03:33 +0300

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,9 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
 
 
+PAGE_SIZE = 6
+
+
 class RecipePagination(PageNumberPagination):
     """Пагинация рецептов."""
 
-    page_size = 6
+    page_size = PAGE_SIZE
     page_size_query_param = 'limit'
-    max_page_size = 20
