b5397fe38e8bd770951ad7a3d551c3541a114891
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-21 14:13:17 +0300

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,9 +1,6 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAuthenticatedOrReadOnly
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or request.user == obj.author
