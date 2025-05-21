25646c19fc76041487d3eca7b0219adf35f7548c
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-21 10:45:21 +0300

    Update urls.py

--- backend/api/recipes/urls.py
+++ backend/api/recipes/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import IngredientViewSet, RecipeViewSet
 
 router = DefaultRouter()
-router.register(r'ingredients', IngredientViewSet, basename='ingredients')
-router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
