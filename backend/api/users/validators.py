bfe2d60af62d099f18b098f01a5485e924da0b43
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-22 15:33:40 +0300

    Add files via upload

--- backend/api/users/validators.py
+++ backend/api/users/validators.py
@@ -1,9 +1,13 @@
 import re
+
+from foodgram.constants import REGEX_NAME, RESERVED_USERNAMES
 from rest_framework.serializers import ValidationError
 
 
-def validate_username(name):
-    """Проверка никнейма"""
-    if name == 'me' or not re.match(r'^[\w.@+-]+$', name):
-        raise ValidationError('Некорректный никнейм')
-    return name
+def validate_username(value):
+    """Проверка никнейма."""
+    if not re.match(REGEX_NAME, value):
+        raise ValidationError("Некорректный никнейм.")
+    elif value in RESERVED_USERNAMES:
+        raise ValidationError("Это имя зарезервировано.")
+    return value
