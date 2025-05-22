0343a272ec7196c203979896d71de1403641c3eb
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-22 15:29:45 +0300

    Add files via upload

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,11 +1,9 @@
+from foodgram.constants import PAGE_SIZE
 from rest_framework.pagination import PageNumberPagination
-
-
-PAGE_SIZE = 6
 
 
 class RecipePagination(PageNumberPagination):
     """Пагинация рецептов."""
 
     page_size = PAGE_SIZE
-    page_size_query_param = 'limit'
+    page_size_query_param = "limit"
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 
-
 urlpatterns = [
-    path('', include('api.recipes.urls')),
-    path('', include('api.users.urls')),
-    path('api/', include('djoser.urls.authtoken'))
+    path("", include("api.recipes.urls")),
+    path("", include("api.users.urls")),
+    path("api/", include("djoser.urls.authtoken")),
 ]
