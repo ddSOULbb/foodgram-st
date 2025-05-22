5476d79ef9f212d0c42d235ce397c56edaefaf8e
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-22 15:31:29 +0300

    Add files via upload

--- backend/api/recipes/filters.py
+++ backend/api/recipes/filters.py
@@ -1,39 +1,41 @@
 import django_filters
-from recipes.models import Recipe, Ingredient
 from django_filters import rest_framework as filters
+from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(filters.FilterSet):
-    """Фидьтр рецептов"""
+    """Фильтр рецептов"""
 
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+    is_favorited = filters.BooleanFilter(method="filter_boolean_field")
+    is_in_shopping_cart = filters.BooleanFilter(method="filter_boolean_field")
 
     class Meta:
         model = Recipe
-        fields = ['author']
+        fields = ["author"]
 
-    def filter_is_favorited(self, queryset, name, value):
-        if self.request.user.is_authenticated:
-            if value:
-                return queryset.filter(favorites__user=self.request.user)
-            return queryset.exclude(favorites__user=self.request.user)
-        return queryset.none() if value else queryset
+    def filter_boolean_field(self, queryset, name, value):
+        if not self.request.user.is_authenticated:
+            return queryset.none() if value else queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        if self.request.user.is_authenticated:
-            if value:
-                return queryset.filter(shoppingcarts__user=self.request.user)
-            return queryset.exclude(shoppingcarts__user=self.request.user)
-        return queryset.none() if value else queryset
+        filter_map = {
+            "is_favorited": "favorites__user",
+            "is_in_shopping_cart": "shoppingcarts__user",
+        }
+
+        filter_field = filter_map.get(name)
+        if not filter_field:
+            return queryset
+
+        if value:
+            return queryset.filter(**{filter_field: self.request.user})
+        return queryset.exclude(**{filter_field: self.request.user})
 
 
 class IngredientFilter(django_filters.FilterSet):
     """Фильтр ингридиентов."""
 
-    name = django_filters.CharFilter(lookup_expr='contains')
+    name = django_filters.CharFilter(lookup_expr="contains")
 
     class Meta:
         model = Ingredient
-        fields = ['name']
+        fields = ["name"]
