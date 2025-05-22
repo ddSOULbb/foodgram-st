01b49558ad225a2c68892c87ba5745d8dc166602
Vladimir Popov <112638108+ddSOULbb@users.noreply.github.com>
2025-05-22 15:27:53 +0300

    Add files via upload

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
-from .models import User, Subscription
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import Subscription, User
 
 
 @admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name',
-                    'last_name', 'is_active', 'is_staff')
-    search_fields = ('username', 'email')
-    search_help_text = 'Поиск по нику или email'
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('id',)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "recipes_count",
+        "subscribers_count",
+    )
+    search_fields = ("username", "email")
+    search_help_text = "Поиск по нику или email"
+    list_filter = ("is_staff", "is_active")
+    ordering = ("id",)
 
     @admin.display(description="Блокировка")
     def is_active_display(self, obj):
         return obj.is_active
 
+    @admin.display(description="Рецептов")
+    def recipes_count(self, obj):
+        return obj.recipes.count()
+
+    @admin.display(description="Подписчиков")
+    def subscribers_count(self, obj):
+        return obj.followers.count()
+
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'subscribed_to')
-    search_fields = ('user__username', 'subscribed_to__username')
-    list_filter = ('user',)
+    list_display = ("id", "user", "subscribed_to")
+    search_fields = ("user__username", "subscribed_to__username")
+    list_filter = ("user",)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,40 +1,40 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-from .constants import MAX_LENGTH_EMAIL, MAX_LENGTH_USERNAME, REGEX_NAME
+from django.db import models
+from foodgram.constants import (MAX_LENGTH_EMAIL, MAX_LENGTH_USERNAME,
+                                REGEX_NAME)
 
 
 class User(AbstractUser):
     """Модель пользователя."""
 
     email = models.EmailField(
-        unique=True, blank=True, max_length=MAX_LENGTH_EMAIL,
-        verbose_name='Электронная почта'
+        unique=True,
+        blank=True,
+        max_length=MAX_LENGTH_EMAIL,
+        verbose_name="Электронная почта",
     )
     username = models.CharField(
-        max_length=MAX_LENGTH_USERNAME, blank=True, unique=True,
-        verbose_name='Имя пользователя',
+        max_length=MAX_LENGTH_USERNAME,
+        blank=True,
+        unique=True,
+        verbose_name="Имя пользователя",
         validators=[
             RegexValidator(
                 regex=REGEX_NAME,
-                message='Никнейм может содержать только буквы и цифры',
-                code='invalid_username'
+                message="Никнейм может содержать только буквы и цифры",
+                code="invalid_username",
             )
-        ]
+        ],
     )
-    avatar = models.ImageField(
-        upload_to='users/',
-        null=True,
-        blank=True,
-        default=None
-    )
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    avatar = models.ImageField(upload_to="users/", null=True, blank=True, default=None)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     class Meta:
-        verbose_name = 'профиль пользователя'
-        verbose_name_plural = 'профили пользователей'
-        ordering = ('username',)
+        verbose_name = "профиль пользователя"
+        verbose_name_plural = "профили пользователей"
+        ordering = ("username",)
 
     def __str__(self):
         return self.username
@@ -44,30 +44,29 @@ class Subscription(models.Model):
     subscribed_to = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='subscribed_to',
-        verbose_name='Подписан на',
+        related_name="subscribed_to",
+        verbose_name="Подписан на",
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='subscriber',
-        verbose_name='Подписчик',
+        related_name="subscriber",
+        verbose_name="Подписчик",
     )
 
     class Meta:
-        verbose_name = 'подписка'
-        verbose_name_plural = 'подписки'
-        ordering = ('user',)
+        verbose_name = "подписка"
+        verbose_name_plural = "подписки"
+        ordering = ("user",)
         constraints = [
             models.UniqueConstraint(
-                fields=['user', 'subscribed_to'],
-                name='unique_subscriber_subscription'
+                fields=["user", "subscribed_to"], name="unique_subscriber_subscription"
             ),
             models.CheckConstraint(
-                check=~models.Q(user=models.F('subscribed_to')),
-                name='prevent_self_subscription',
-            )
+                check=~models.Q(user=models.F("subscribed_to")),
+                name="prevent_self_subscription",
+            ),
         ]
 
     def __str__(self):
-        return f'Подписчик {self.user}'
+        return f"Подписчик {self.user}"
